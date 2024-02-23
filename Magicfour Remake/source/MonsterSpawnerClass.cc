efe222ed9d93b0a3448987bf703ff579cce508ca
firstin0907 <gisub.sin@sankun.com>
2024-02-23 15:34:49 +0900

    Feat: Spawn monsters more precisely

--- Magicfour Remake/source/MonsterSpawnerClass.cc
+++ Magicfour Remake/source/MonsterSpawnerClass.cc
@@ -19,42 +19,44 @@ void MonsterSpawnerClass::Frame(time_t curr_time, time_t delta_time,
 	// When Gamelevel is up, plan which and when the monster will be spawned.
 	const unsigned __int64 curr_game_level = (curr_time + (kLevelupTerm - kSpawnBeginTime)) / kLevelupTerm;
 
-	for (; schedule_iterator_ != monster_spawn_schedule_.end()
-		&& curr_time >= schedule_iterator_->first; schedule_iterator_++)
+	do
 	{
-		direction_t direction = RandomClass::rand(2) ? LEFT_FORWARD : RIGHT_FORWARD;
-
-		switch (schedule_iterator_->second = 1)
+		for (; schedule_iterator_ != monster_spawn_schedule_.end()
+			&& curr_time >= schedule_iterator_->first; schedule_iterator_++)
 		{
-		case 0:
-			monsters_.emplace_back(new MonsterOctopus(direction, schedule_iterator_->first));
-			break;
-		case 1:
-			monsters_.emplace_back(new MonsterDuck(direction, schedule_iterator_->first));
-			break;
-		case 2:
-			monsters_.emplace_back(new MonsterBird(direction, schedule_iterator_->first));
-			break;
-		case 3:
-			monsters_.emplace_back(new MonsterStop(schedule_iterator_->first));
-			break;
+			direction_t direction = RandomClass::rand(2) ? LEFT_FORWARD : RIGHT_FORWARD;
+
+			switch (schedule_iterator_->second)
+			{
+			case 0:
+				monsters_.emplace_back(new MonsterOctopus(direction, schedule_iterator_->first));
+				break;
+			case 1:
+				monsters_.emplace_back(new MonsterDuck(direction, schedule_iterator_->first));
+				break;
+			case 2:
+				monsters_.emplace_back(new MonsterBird(direction, schedule_iterator_->first));
+				break;
+			case 3:
+				monsters_.emplace_back(new MonsterStop(schedule_iterator_->first));
+				break;
+			}
 		}
-	}
 
-	// TODO : the case where leveling is pended due to low frame rate. (over 10second)
-	if (game_level_ < curr_game_level)
-	{
-		const time_t levelup_time = game_level_++ * kLevelupTerm + kSpawnBeginTime;
-
-		monster_spawn_schedule_.clear();		
-		
-		for (int i = 0; i < 3 + game_level_; i++)
+		if (game_level_ < curr_game_level)
 		{
-			monster_spawn_schedule_.emplace_back(
-				RandomClass::rand(levelup_time, levelup_time + kLevelupTerm), RandomClass::rand(4));
-		}
-		sort(monster_spawn_schedule_.begin(), monster_spawn_schedule_.end());
+			const time_t levelup_time = (game_level_++) * kLevelupTerm + kSpawnBeginTime;
 
-		schedule_iterator_ = monster_spawn_schedule_.begin();
-	}
+			monster_spawn_schedule_.clear();
+
+			for (int i = 0; i < 6 + game_level_; i++)
+			{
+				monster_spawn_schedule_.emplace_back(
+					RandomClass::rand(levelup_time + 100, levelup_time + kLevelupTerm - 100), RandomClass::rand(4));
+			}
+			sort(monster_spawn_schedule_.begin(), monster_spawn_schedule_.end());
+
+			schedule_iterator_ = monster_spawn_schedule_.begin();
+		}
+	} while (game_level_ < curr_game_level);
 }
