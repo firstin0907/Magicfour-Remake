cd64d9348d402c77e291c4653a7f39716336ee79
firstin0907 <bbrero62@gmail.com>
2026-01-25 02:27:05 +0900

    Refactor: Remove raw pointer in AnimatedObjectClass
    - Substitute the raw pointers with the smart pointers in AnimatedObjectClass.

--- Magicfour Remake/include/core/AnimatedObjectClass.hh
+++ Magicfour Remake/include/core/AnimatedObjectClass.hh
@@ -2,84 +2,77 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <xstring>
 #include <unordered_map>
+#include <fstream>
 
 #include <DirectXMath.h>
 
 enum channel_t
 {
-	ANIMATION_CHANNEL_XPOS,
-	ANIMATION_CHANNEL_YPOS,
-	ANIMATION_CHANNEL_ZPOS,
-	ANIMATION_CHANNEL_XROT,
-	ANIMATION_CHANNEL_YROT,
-	ANIMATION_CHANNEL_ZROT
+    ANIMATION_CHANNEL_XPOS,
+    ANIMATION_CHANNEL_YPOS,
+    ANIMATION_CHANNEL_ZPOS,
+    ANIMATION_CHANNEL_XROT,
+    ANIMATION_CHANNEL_YROT,
+    ANIMATION_CHANNEL_ZROT
 };
 
 class AnimatedObjectClass
 {
 public:
-	using FrameShape = std::unordered_map<std::string, DirectX::XMMATRIX>;
+    using FrameShape = std::unordered_map<std::string, DirectX::XMMATRIX>;
+    
+    struct AnimationNode
+    {
+        float offset_x, offset_y, offset_z;
 
-private:
-	using XMMATRIX = DirectX::XMMATRIX;
+        std::string name;
+        std::vector<channel_t> channels;
 
-	template<typename T>
-	using vector = std::vector<T>;
+        std::weak_ptr<AnimationNode> parent;
+        std::vector<std::shared_ptr<AnimationNode>> children;
 
-	struct AnimationNode
-	{
-		float offset_x, offset_y, offset_z;
+        DirectX::XMMATRIX link_matrix;
+        DirectX::XMMATRIX shape_transform;
+        DirectX::XMMATRIX global_transform;
 
-		std::string name;
-		int channel_num;
-		channel_t* channels;
+        AnimationNode(std::string name, std::weak_ptr<AnimationNode> parent = std::weak_ptr<AnimationNode>())
+            : name(name), parent(parent)
+        {
+            global_transform = link_matrix = DirectX::XMMatrixIdentity();
+        }
 
-		AnimationNode* parent;
-		vector<AnimationNode*> children;
-
-		XMMATRIX link_matrix;
-		XMMATRIX shape_transform;
-		XMMATRIX global_transform;
-
-		AnimationNode(std::string name, AnimationNode* parent)
-			: name(name), parent(parent), channels(nullptr)
-		{
-			global_transform = link_matrix = DirectX::XMMatrixIdentity();
-		};
-		~AnimationNode() { delete[] channels; }
-	};
-
-	int channels_num, frames_num;
-	float frame_time;
-
-	AnimationNode root;
-	vector<AnimationNode* > nodes;
-
-	float* frame_info;
-
-
-private:
-	AnimationNode* create_hierarchy(
-		AnimationNode* curr_node, std::ifstream& fin);
+        ~AnimationNode() = default;
+    };
 
 public:
-	/// @brief Merges two frame shapes with linear interpolation.
-	/// @param first The first frame shape.
-	/// @param second The second frame shape.
-	/// @param alpha The interpolation factor (0.0 to 1.0). Alpha = 0.0 returns the first shape, alpha = 1.0 returns the second shape.
-	/// @return The merged frame shape.
-	static AnimatedObjectClass::FrameShape MergeFrameShapes(
-		const AnimatedObjectClass::FrameShape& first,
-		const AnimatedObjectClass::FrameShape& second,
-		float alpha
-	);
+    AnimatedObjectClass(const char* filename, bool ignore_first_frame = false);
+    ~AnimatedObjectClass();
 
-	FrameShape UpdateAndGetShapeMatrix(const int frame, XMMATRIX transform_of_root);
+    /// @brief Merges two frame shapes with linear interpolation.
+    /// @param first The first frame shape.
+    /// @param second The second frame shape.
+    /// @param alpha The interpolation factor (0.0 to 1.0).
+    ///              Alpha = 0.0 returns the first shape, alpha = 1.0 returns the second shape.
+    /// @return The merged frame shape.
+    static FrameShape MergeFrameShapes(
+        const FrameShape& first,
+        const FrameShape& second,
+        float alpha
+    );
 
-	AnimatedObjectClass(const char* filename, bool ignore_first_frame = false);
-	~AnimatedObjectClass();
+    FrameShape UpdateAndGetShapeMatrix(const int frame, DirectX::XMMATRIX transform_of_root);
+
+private:
+    std::shared_ptr<AnimationNode> create_hierarchy(
+        std::shared_ptr<AnimationNode> curr_node, std::ifstream& fin);
+
+private:
+    std::wstring filename_;
+    int channels_num_, frames_num_;
+    float frame_time_;
+
+    std::vector<std::shared_ptr<AnimationNode>> nodes_;
+    std::vector<float> frame_info_;
 };
-
-
-
--- Magicfour Remake/source/core/AnimatedObjectClass.cc
+++ Magicfour Remake/source/core/AnimatedObjectClass.cc
@@ -1,6 +1,7 @@
 #include "core/AnimatedObjectClass.hh"
 
 #include <fstream>
+#include <cstring>
 
 #include "core/GameException.hh"
 
@@ -11,141 +12,8 @@
 using namespace std; 
 using namespace DirectX;
 
-AnimatedObjectClass::AnimationNode*
-	AnimatedObjectClass::create_hierarchy(
-		AnimationNode* curr_node, ifstream& fin)
-{
-	string buffer;
-	fin >> buffer; // {
-
-	while (1)
-	{
-		fin >> buffer;
-		if (buffer == "OFFSET")
-		{
-			fin >> curr_node->offset_x >> curr_node->offset_y >> curr_node->offset_z;
-			curr_node->link_matrix = XMMatrixTranslation(
-				curr_node->offset_x, curr_node->offset_y, curr_node->offset_z);
-		}
-
-		else if (buffer == "CHANNELS")
-		{
-			fin >> curr_node->channel_num;
-			channels_num += curr_node->channel_num;
-			curr_node->channels = new channel_t[curr_node->channel_num];
-
-			for (int i = 0; i < curr_node->channel_num; i++)
-			{
-				fin >> buffer;
-				if (buffer.size() > 2)
-				{
-					switch (buffer[0])
-					{
-					case 'X':
-						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_XPOS;
-						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_XROT;
-						break;
-					case 'Y':
-						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_YPOS;
-						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_YROT;
-						break;
-					case 'Z':
-						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_ZPOS;
-						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_ZROT;
-						break;
-					}
-				}
-			}
-		}
-		else if (buffer == "JOINT" || buffer == "End")
-		{
-			fin >> buffer;
-
-			AnimationNode* child_node = new AnimationNode(buffer, curr_node);
-			nodes.push_back(child_node);
-
-			curr_node->children.push_back(create_hierarchy(child_node, fin));
-		}
-		else if (buffer == "}") break;
-	}
-
-	
-
-	if (!curr_node->children.empty())
-	{
-		auto& child = curr_node->children[0];
-
-		XMVECTOR child_offset = { child->offset_x, child->offset_y, child->offset_z };
-		float length = sqrt(child->offset_x * child->offset_x + child->offset_y * child->offset_y
-			+ child->offset_z * child->offset_z);
-
-		child_offset = child_offset / length;
-
-		auto axis = XMVector3Cross({ 0, 1, 0 }, child_offset);
-		float angle = acos(child->offset_y / length);
-
-		if (XMVector3Equal(axis, XMVectorZero())) axis = { 0, 1, 0 }, angle = 0;
-
-		curr_node->shape_transform = XMMatrixTranslation(0, 1, 0)
-			* XMMatrixScaling(0.125f, length / 2, 0.125f) * XMMatrixRotationAxis(axis, angle);
-	}
-
-	return curr_node;
-
-}
-
-AnimatedObjectClass::~AnimatedObjectClass()
-{
-	for (auto& node : nodes) delete node;
-	nodes.clear();
-
-	delete[] frame_info;
-}
-
-AnimatedObjectClass::FrameShape AnimatedObjectClass::UpdateAndGetShapeMatrix(const int frame, XMMATRIX transform_of_root)
-{
-	FrameShape result;
-
-	float* frame_info_it = frame_info + (frame % frames_num) * channels_num;
-
-	root.global_transform = transform_of_root;
-
-	for (auto& node : nodes)
-	{
-		XMMATRIX joint_transform = XMMatrixIdentity();
-		for (int i = 0; i < node->channel_num; i++)
-		{
-			switch (node->channels[i])
-			{
-			case ANIMATION_CHANNEL_XPOS:
-				joint_transform = XMMatrixTranslation(*frame_info_it++, 0, 0) * joint_transform; break;
-			case ANIMATION_CHANNEL_YPOS:
-				joint_transform = XMMatrixTranslation(0, *frame_info_it++, 0) * joint_transform; break;
-			case ANIMATION_CHANNEL_ZPOS:
-				joint_transform = XMMatrixTranslation(0, 0, *frame_info_it++ ) * joint_transform; break;
-				break;
-			case ANIMATION_CHANNEL_XROT:
-				joint_transform = XMMatrixRotationX(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
-			case ANIMATION_CHANNEL_YROT:
-				joint_transform = XMMatrixRotationY(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
-			case ANIMATION_CHANNEL_ZROT:
-				joint_transform = XMMatrixRotationZ(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
-				break;
-				
-			}
-		}
-		node->global_transform = joint_transform * node->link_matrix * node->parent->global_transform;
-		if (!node->children.empty())
-		{
-			result[node->name] = node->shape_transform * node->global_transform;
-		}
-	}
-
-	return result;
-}
-
 AnimatedObjectClass::AnimatedObjectClass(const char* filename, bool ignore_first_frame)
-	: channels_num(0), frame_info(nullptr), root("", nullptr)
+	: channels_num_(0), filename_(std::wstring(filename, filename + strlen(filename)))
 {
 	ifstream fin(filename);
 	if (fin.fail()) throw filenotfound_error(filename, WFILE, __LINE__);
@@ -156,30 +24,44 @@ AnimatedObjectClass::AnimatedObjectClass(const char* filename, bool ignore_first
 	fin >> buffer; // name
 
 	// Create Tree
-	AnimationNode* root_node = new AnimationNode(buffer, &root);
-	nodes.push_back(root_node);
-	create_hierarchy(root_node, fin);
+	nodes_.push_back(std::make_shared<AnimationNode>(buffer));
+	create_hierarchy(*nodes_.begin(), fin);
 
 	fin >> buffer; // MOTION
 	fin >> buffer; // Frames:
-	fin >> frames_num;
+	fin >> frames_num_;
 
 	fin >> buffer; // Frame
 	fin >> buffer; // Time:
-	fin >> frame_time;
+	fin >> frame_time_;
 
-	if(ignore_first_frame)
+	if (ignore_first_frame)
 	{
-		--frames_num;
-		for (int i = 0; i < channels_num; i++) fin >> buffer; // skip first frame
+		--frames_num_;
+		for (int i = 0; i < channels_num_ && fin >> buffer; i++); // skip first frame
 	}
 
-	const int info_sz = frames_num * channels_num;
-	frame_info = new float[info_sz];
-	for (int i = 0; i < info_sz; i++) fin >> frame_info[i];
+	const int info_sz = frames_num_ * channels_num_;
+	if (info_sz <= 0)
+	{
+		throw GameException(
+			L"AnimatedObjectClass: No frame data in file(\"" + this->filename_ + L"\").",
+			WFILE, __LINE__);
+	}
 
-	fin.close();
+	frame_info_.assign(info_sz, 0.0f);
+	for (int i = 0; i < info_sz; i++)
+	{
+		if (!(fin >> frame_info_[i]))
+		{
+			throw GameException(
+				L"AnimatedObjectClass: Unexpected end of file(\"" + this->filename_ + L"\") when reading frame data.",
+				WFILE, __LINE__);
+		}
+	}
 }
+
+AnimatedObjectClass::~AnimatedObjectClass() = default;
 
 AnimatedObjectClass::FrameShape AnimatedObjectClass::MergeFrameShapes(
 	const AnimatedObjectClass::FrameShape& first,
@@ -208,4 +90,134 @@ AnimatedObjectClass::FrameShape AnimatedObjectClass::MergeFrameShapes(
 
 	return result;
 
+}
+
+AnimatedObjectClass::FrameShape AnimatedObjectClass::UpdateAndGetShapeMatrix(const int frame, XMMATRIX transform_of_root)
+{
+	if (nodes_.empty()) return {};
+
+	FrameShape result;
+
+	std::vector<float>::iterator frame_info_it = frame_info_.begin() + (frame % frames_num_) * channels_num_;
+
+	nodes_[0]->global_transform = transform_of_root;
+
+	for (auto& node : nodes_)
+	{
+		XMMATRIX joint_transform = XMMatrixIdentity();
+		for (channel_t channel : node->channels)
+		{
+			switch (channel)
+			{
+			case ANIMATION_CHANNEL_XPOS:
+				joint_transform = XMMatrixTranslation(*frame_info_it++, 0, 0) * joint_transform; break;
+			case ANIMATION_CHANNEL_YPOS:
+				joint_transform = XMMatrixTranslation(0, *frame_info_it++, 0) * joint_transform; break;
+			case ANIMATION_CHANNEL_ZPOS:
+				joint_transform = XMMatrixTranslation(0, 0, *frame_info_it++) * joint_transform; break;
+			case ANIMATION_CHANNEL_XROT:
+				joint_transform = XMMatrixRotationX(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
+			case ANIMATION_CHANNEL_YROT:
+				joint_transform = XMMatrixRotationY(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
+			case ANIMATION_CHANNEL_ZROT:
+				joint_transform = XMMatrixRotationZ(*frame_info_it++ * 0.0174532925f) * joint_transform; break;
+				break;
+
+			}
+		}
+
+		auto parent = node->parent.lock();
+		if (!parent) parent = node;
+
+		node->global_transform = joint_transform * node->link_matrix * parent->global_transform;
+		if (!node->children.empty())
+		{
+			result[node->name] = node->shape_transform * node->global_transform;
+		}
+	}
+
+	return result;
+}
+
+std::shared_ptr<AnimatedObjectClass::AnimationNode>
+AnimatedObjectClass::create_hierarchy(
+	std::shared_ptr<AnimationNode> curr_node, ifstream& fin)
+{
+	string buffer;
+	fin >> buffer; // {
+
+	while (1)
+	{
+		fin >> buffer;
+		if (buffer == "OFFSET")
+		{
+			fin >> curr_node->offset_x >> curr_node->offset_y >> curr_node->offset_z;
+			curr_node->link_matrix = XMMatrixTranslation(
+				curr_node->offset_x, curr_node->offset_y, curr_node->offset_z);
+		}
+
+		else if (buffer == "CHANNELS")
+		{
+			int node_channel_num;
+			fin >> node_channel_num;
+			channels_num_ += node_channel_num;
+			curr_node->channels.resize(node_channel_num);
+
+			for (int i = 0; i < node_channel_num; i++)
+			{
+				fin >> buffer;
+				if (buffer.size() > 2)
+				{
+					switch (buffer[0])
+					{
+					case 'X':
+						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_XPOS;
+						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_XROT;
+						break;
+					case 'Y':
+						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_YPOS;
+						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_YROT;
+						break;
+					case 'Z':
+						if (buffer[1] == 'p') curr_node->channels[i] = ANIMATION_CHANNEL_ZPOS;
+						else if (buffer[1] == 'r') curr_node->channels[i] = ANIMATION_CHANNEL_ZROT;
+						break;
+					}
+				}
+			}
+		}
+		else if (buffer == "JOINT" || buffer == "End")
+		{
+			fin >> buffer;
+
+			auto child_node = std::make_shared<AnimationNode>(buffer, curr_node);
+			nodes_.push_back(child_node);
+
+			curr_node->children.push_back(create_hierarchy(child_node, fin));
+		}
+		else if (buffer == "}") break;
+	}
+
+
+
+	if (!curr_node->children.empty())
+	{
+		auto& child = curr_node->children[0];
+
+		XMVECTOR child_offset = { child->offset_x, child->offset_y, child->offset_z };
+		float length = sqrt(child->offset_x * child->offset_x + child->offset_y * child->offset_y
+			+ child->offset_z * child->offset_z);
+
+		child_offset = child_offset / length;
+
+		auto axis = XMVector3Cross({ 0, 1, 0 }, child_offset);
+		float angle = acos(child->offset_y / length);
+
+		if (XMVector3Equal(axis, XMVectorZero())) axis = { 0, 1, 0 }, angle = 0;
+
+		curr_node->shape_transform = XMMatrixTranslation(0, 1, 0)
+			* XMMatrixScaling(0.125f, length / 2, 0.125f) * XMMatrixRotationAxis(axis, angle);
+	}
+
+	return curr_node;
 }
